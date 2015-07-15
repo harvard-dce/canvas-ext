38ac5494906a90435aa711cfdffff913e6d5a187
Jim Kang <jimkang@gmail.com>
2015-07-15 14:07:06 -0400

    Added hideSyllabusTableIfFlagExists to support hiding the table if the user puts an html element with the id 'hide-syllabus-table' in their syllabus description. Wrapped everything in an IIFE to avoid polluting global space.

--- ext.js
+++ ext.js
@@ -1,33 +1,46 @@
-
-
-window.addEventListener('message', handleMessage, false);
-
-function handleMessage(e) {
-  try {
-    if (e.data && e.data.height.length > 2 && e.data.request === 'changeHeight') {
-      console.log('resizing to : ' + e.data.height);
-      var iframe = findIframe(e.data.href);
-      if (iframe) {
-        iframe.style.height = e.data.height;
-     }
-      else {
-        console.log('Could not find iframe with href', e.data.href);
+((function extScope() {
+  function handleMessage(e) {
+    try {
+      if (e.data && e.data.height.length > 2 && e.data.request === 'changeHeight') {
+        console.log('resizing to : ' + e.data.height);
+        var iframe = findIframe(e.data.href);
+        if (iframe) {
+          iframe.style.height = e.data.height;
+       }
+        else {
+          console.log('Could not find iframe with href', e.data.href);
+        }
+      } else {
+        console.log('Missing e.data or e.data.request or proper e.data.height');
       }
-    } else {
-      console.log('Missing e.data or e.data.request or proper e.data.height');
+    } catch (e){
+      console.log('Caught error: ' , e, e.stack);
     }
-  } catch (e){
-    console.log('Caught error: ' , e, e.stack);
   }
-}
 
-function findIframe(href) {
-  var iframes = document.querySelectorAll('iframe');
-  for (var i = 0; i < iframes.length; ++i) {
-    var iframe = iframes[i];
-    if (iframe.src === href) {
-      return iframe;
+  function findIframe(href) {
+    var iframes = document.querySelectorAll('iframe');
+    for (var i = 0; i < iframes.length; ++i) {
+      var iframe = iframes[i];
+      if (iframe.src === href) {
+        return iframe;
+      }
     }
   }
-}
-  
+
+  function hideSyllabusTableIfFlagExists() {
+    var flag = document.querySelector('#hide-syllabus-table');
+    if (flag) {
+      var syllabusEl = document.querySelector('#syllabusContainer');
+      if (syllabusEl) {
+        syllabusEl.parentElement.removeChild(syllabusEl);
+      }
+    }
+  }
+
+  ((function init() {
+    window.addEventListener('message', handleMessage, false);
+    hideSyllabusTableIfFlagExists();
+  })());
+
+})());
